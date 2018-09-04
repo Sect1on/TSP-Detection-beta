252fbfced7c7274b0d86c7612c7ea2a3cbe38057
Sect1on <948253398@qq.com.com>
2018-09-04 15:50:54 +0800

    基础功能实现

---
+++ TSP-Detection/mainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TSP_Detection
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOrigin = new System.Windows.Forms.PictureBox();
+            this.picROI = new System.Windows.Forms.PictureBox();
+            this.picGray = new System.Windows.Forms.PictureBox();
+            this.picGrayHist = new System.Windows.Forms.PictureBox();
+            this.picHist = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOrigin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picROI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGray)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGrayHist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOrigin
+            // 
+            this.picOrigin.Location = new System.Drawing.Point(40, 12);
+            this.picOrigin.Name = "picOrigin";
+            this.picOrigin.Size = new System.Drawing.Size(428, 266);
+            this.picOrigin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOrigin.TabIndex = 0;
+            this.picOrigin.TabStop = false;
+            // 
+            // picROI
+            // 
+            this.picROI.Location = new System.Drawing.Point(486, 12);
+            this.picROI.Name = "picROI";
+            this.picROI.Size = new System.Drawing.Size(575, 50);
+            this.picROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picROI.TabIndex = 2;
+            this.picROI.TabStop = false;
+            // 
+            // picGray
+            // 
+            this.picGray.Location = new System.Drawing.Point(486, 68);
+            this.picGray.Name = "picGray";
+            this.picGray.Size = new System.Drawing.Size(575, 50);
+            this.picGray.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picGray.TabIndex = 3;
+            this.picGray.TabStop = false;
+            // 
+            // picGrayHist
+            // 
+            this.picGrayHist.Location = new System.Drawing.Point(486, 124);
+            this.picGrayHist.Name = "picGrayHist";
+            this.picGrayHist.Size = new System.Drawing.Size(575, 111);
+            this.picGrayHist.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picGrayHist.TabIndex = 6;
+            this.picGrayHist.TabStop = false;
+            // 
+            // picHist
+            // 
+            this.picHist.Location = new System.Drawing.Point(486, 241);
+            this.picHist.Name = "picHist";
+            this.picHist.Size = new System.Drawing.Size(575, 115);
+            this.picHist.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHist.TabIndex = 7;
+            this.picHist.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(40, 303);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 456);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picHist);
+            this.Controls.Add(this.picGrayHist);
+            this.Controls.Add(this.picGray);
+            this.Controls.Add(this.picROI);
+            this.Controls.Add(this.picOrigin);
+            this.Name = "mainForm";
+            this.Text = "TSP-Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.picOrigin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picROI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGray)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGrayHist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOrigin;
+        private System.Windows.Forms.PictureBox picROI;
+        private System.Windows.Forms.PictureBox picGray;
+        private System.Windows.Forms.PictureBox picGrayHist;
+        private System.Windows.Forms.PictureBox picHist;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ TSP-Detection/mainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.Util;
+
+
+namespace TSP_Detection
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        Image<Bgr, byte> frame1 = new Image<Bgr, byte>(@"..\..\..\Pictures\demo3-3.jpg");//从文件加载图片
+        Image<Bgr, byte> frame2 = new Image<Bgr, byte>(@"..\..\..\Pictures\demo3-3.jpg");//从文件加载图片
+        Image<Bgr, byte> histpic = new Image<Bgr, byte>(1930, 100, new Bgr(255, 255, 255));
+        Image<Bgr, byte> grayhistpic = new Image<Bgr, byte>(1930, 306, new Bgr(255, 255, 255));
+
+        public void SetRoiRed(Image<Bgr, byte> image, Rectangle roi)//ROI选取函数
+        {
+            image.ROI = roi;
+            //image.ROI = Rectangle.Empty;
+        }
+
+        public void startpoints(Image<Bgr, byte> image1, Image<Bgr, byte> image2)
+        {
+            int n1, n2, gray1, gray2, gray3;
+            int average = 0;
+            int[] graygrads = new int[1920];
+            int[] gray = new int[1920];
+            int calnumber1 = 0;
+            int calnumber2 = 0;
+            int calsum1 = 0;
+            int calsum2 = 0;
+            int caltranfer1 = 0;
+            int caltranfer2 = 0;
+
+            //picOrigin.Image = image2.Bitmap;
+            image2.Draw(new Rectangle(450, 1037, 1050, 40), new Bgr(Color.FromArgb(255, Color.Blue)), 3);
+            SetRoiRed(image1, new Rectangle(450, 1037, 1050, 40));
+            picROI.Image = image1.Bitmap;
+            var grayImage = image1.Convert<Gray, Byte>();
+            picGray.Image = grayImage.Bitmap;
+
+            for (n1 = 0; n1 < 1045; n1++)
+            {
+                gray1 = 0;
+                gray2 = 0;
+                for (n2 = 0; n2 < 40; n2++)
+                {
+                    //灰度值计算
+                    Color pix1 = image1.Bitmap.GetPixel(n1, n2);
+                    Color pix2 = image1.Bitmap.GetPixel(n1 + 1, n2);
+                    gray1 = gray1 + (int)(pix1.R * 0.30 + pix1.G * 0.59 + pix1.B * 0.11);
+                    gray2 = gray2 + (int)(pix2.R * 0.30 + pix2.G * 0.59 + pix2.B * 0.11);
+                }
+                gray1 = gray1 / 20;
+                gray2 = gray2 / 20;
+                //梯度计算
+                gray1 = Math.Abs(gray1 - gray2);
+                graygrads[n1] = gray1;
+                average = average + gray1;
+
+            }
+
+            average = average / 1045 + 1;
+            for (n1 = 0; n1 < 1044; n1++)
+            {
+                if (graygrads[n1] <= average)
+                {
+                    graygrads[n1] = 0;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for (n1 = 0; n1 < 958; n1++)
+            {
+                caltranfer1 = 0;
+                for (n2 = 0; n2 < 80; n2++)
+                {
+                    caltranfer1 = caltranfer1 + graygrads[n1 + n2];
+                }
+                if (caltranfer1 > calsum1)
+                {
+                    calsum1 = caltranfer1;
+                    calnumber1 = n1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for (n1 = 958; n1 < 1835; n1++)
+            {
+                caltranfer2 = 0;
+                for (n2 = 0; n2 < 10; n2++)
+                {
+                    caltranfer2 = caltranfer2 + graygrads[n1 + n2];
+                }
+                if (caltranfer2 > calsum2)
+                {
+                    calsum2 = caltranfer2;
+                    calnumber2 = n1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for (int i = 0; i < 1915; i++)
+            {
+                histpic.Draw(new Rectangle(i, 100 - graygrads[i] * 4, 1, graygrads[i] * 4), new Bgr(Color.FromArgb(255, Color.Blue)), 1);
+            }
+
+            histpic.Draw(new Rectangle(calnumber1, 2, 80, 98), new Bgr(Color.FromArgb(255, Color.Red)), 5);
+            histpic.Draw(new Rectangle(calnumber2, 2, 80, 98), new Bgr(Color.FromArgb(255, Color.Red)), 5);
+
+            image2.Draw(new Rectangle(calnumber1 + 450, 1040, 80, 40), new Bgr(Color.FromArgb(255, Color.Red)), 5);
+            image2.Draw(new Rectangle(calnumber2 + 100, 1040, 80, 40), new Bgr(Color.FromArgb(255, Color.Red)), 5);
+
+            picOrigin.Image = image2.Bitmap;
+
+
+            for (n1 = 0; n1 < 1045; n1++)
+            {
+                gray3 = 0;
+                //Color pix3 = image1.Bitmap.GetPixel(n1, n2);
+                for (n2 = 0; n2 < 40; n2++)
+                {
+                    Color pix3 = image1.Bitmap.GetPixel(n1, n2);
+                    gray3 = gray3 + (int)(pix3.R * 0.30 + pix3.G * 0.59 + pix3.B * 0.11);
+
+                }
+                gray3 = gray3 / 20;
+                gray[n1] = gray3;
+                grayhistpic.Draw(new Rectangle(n1, 255 - gray[n1], 1, gray[n1]), new Bgr(Color.FromArgb(255, Color.Blue)), 1);
+            }
+
+
+
+
+            picHist.Image = histpic.Bitmap;
+            picGrayHist.Image = grayhistpic.Bitmap;
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startpoints(frame1, frame2);
+        }
+    }
+}
